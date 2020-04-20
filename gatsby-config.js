fcda070ea7428f16c02cb1431ac8b141db1e5014
dennisclemente <dennisclemente@gmail.com>
2020-04-20 17:49:39 -0400

    added content

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ require("dotenv").config({
 });
 module.exports = {
   siteMetadata: {
-    title: `Regular Joe's`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Kapihan sa Maywood`,
+    description: `E-commerce project`,
     author: `@gatsbyjs`
   },
   plugins: [
